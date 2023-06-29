6e01a87e2d09583babae0271f8bc7a710fa42b3f
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-06-29 11:45:33 +0200

    Removed Express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,9 @@
 import "reflect-metadata";
-import express from "express";
 import { AppDataSource } from "./dataSource";
 import { Ad } from "./entities/ad";
 
-const port = 5050;
-const app = express();
-
-app.use(express.json());
-
-app.post("/api/ad", async (req, res) => {
-  console.log(req.body);
-  const addFromClient = new Ad();
-  addFromClient.content = req.body.content;
-  addFromClient.title = req.body.title;
-  const result = await addFromClient.save();
-  res.status(201).send(result);
-});
-
-app.get("/api/ad", async (_req, res) => {
-  const result = await Ad.find();
-  res.send(result);
-});
-
 const start = async () => {
   await AppDataSource.initialize();
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
 };
 
 start();

075b2ee38900034455f2a373da7f44dab1ac4622
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-06-29 12:06:44 +0200

    Added mutation to add new ad

--- backend/src/resolvers/adResolver.ts
+++ backend/src/resolvers/adResolver.ts
@@ -1,11 +1,23 @@
 import { Ad } from "../entities/ad";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver(Ad)
 export class AdResolver {
   @Query(() => [Ad])
   async getAllAds() {
     const result = Ad.find();
+    return result;
+  }
+
+  @Mutation(() => Ad)
+  async addAd(
+    @Arg("title") titleArg: string,
+    @Arg("content") contentArg: string
+  ) {
+    const adFromClient = new Ad();
+    adFromClient.title = titleArg;
+    adFromClient.content = contentArg;
+    const result = await adFromClient.save();
     return result;
   }
 }

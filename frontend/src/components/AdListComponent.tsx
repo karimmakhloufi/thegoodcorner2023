9b8dd95d26eab0e7125a83795eaa8bdea5be264d
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2023-08-02 10:45:29 +0200

    Displaying fake ad titles in a list

--- frontend/src/components/AdListComponent.tsx
+++ frontend/src/components/AdListComponent.tsx
@@ -1,5 +1,31 @@
 const AdListComponent = () => {
-  return <p>I will be the component</p>;
+  const ads = [
+    {
+      id: 0,
+      title: "bike to sell",
+      price: 100,
+      description: "I will sell you my bike for 10 $",
+    },
+    {
+      id: 1,
+      title: "car to sell",
+      price: 10000,
+      description: "I will sell you my car for 10 000 $",
+    },
+    {
+      id: 2,
+      title: "chair to sell",
+      price: 10,
+      description: "I will sell you my chait for 10 $",
+    },
+    {
+      id: 3,
+      title: "computer to sell",
+      price: 1000,
+      description: "I will sell you my computer for 1 000 $",
+    },
+  ];
+  return ads.map((el) => <p key={el.id}>{el.title}</p>);
 };
 
 export default AdListComponent;
